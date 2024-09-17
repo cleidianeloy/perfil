cb204fa366ec5be845cd33a43f4d6d1efcaedf5c
cleidianeloy <cleidianeloy@hotmail.com>
2024-09-17 19:15:01 -0300

    organizing material - new theme state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import Home from './Home'
 function App() {
+  const [themeDark, setThemeDark] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home themeDark={[themeDark, setThemeDark]}/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+function Home(props){
+    const [themeDark, setThemeDark] = props.themeDark;
+    return (
+        <section>
+            <aside>
+                <button></button>
+                <select></select>
+            </aside>
+            <main>
+                <img/>
+                <h1>Cleidiane da Rosa Eloy</h1>
+                <h2>Desenvolvedora Front-End</h2>
+                <p>Desenvolvedora Web com paixão por tecnologia e experiência em projetos de ensino a distância. Possui habilidades sólidas em javascript, jquery, CSS, HTML e React. Comprometida com a acessibilidade digital e com o desenvolvimento de soluções inovadoras. Proativa, colaborativa e sempre disposta a aprender e crescer profissionalmente.</p>
+
+                <address>
+
+                </address>
+                <a href=""> <span>&#8594;</span> Curriculo</a>
+            </main>
+        </section>
+
+    )
+}
+export default Home;
